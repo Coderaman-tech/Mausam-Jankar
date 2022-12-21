5e529c44eab5c9d942d9b3528d01138b5501ffaa
Aman Kumar Shrivastava <amankumarshrivastava835@gmail.com>
2022-12-22 02:28:21 +0530

    modified:   index.html 	modified:   main.js 	modified:   style.css

--- main.js
+++ main.js
@@ -3,10 +3,20 @@ const API_KEY="ed3bc1def53428b7b7537acd276e8a35";
 
 const DAYS_OF_THE_WEEK=["sun","mon","tue","wed","thu","fri","sat"];
 
+let selectedCityText;
+let selectedCity;
+
+const getCitiesUsingGeolocation=async(searchText)=>{
+   const response= await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchText}&limit=5&appid=${API_KEY}`);
+   return response.json();
+}
+
+ 
+
 //fetch the data about weather with async await to get full response
-const getCurrentWeatherData=async()=>{
-    const city="gopalganj";
-    const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+const getCurrentWeatherData=async({lat,lon,name:city})=>{
+    const url=lat && lon ?`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`:`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    const response= await fetch(url);
     return response.json()
 }
 
@@ -40,15 +50,23 @@ loadCurrentForecast=({name,main:{temp,temp_max,temp_min},weather:[{description}]
 
 //take content from API of forecast and put into a hourly-conatiner id
 
-const loadHourlyForecast=(hourlyForecast)=>{
+const loadHourlyForecast=({main:{temp:tempNow},weather:[{icon:iconNow}]},hourlyForecast)=>{
     // console.log(hourlyForecast);
+    const timeFormatter=Intl.DateTimeFormat("en",{hour12:true,hour:"numeric"});
+
     let dataFor12Hours=hourlyForecast.slice(1,13);
     const hourlyContainer=document.querySelector(".hourly-container");
-    let innerHTMLString=``;
+    let innerHTMLString=` <article>
+    <h3 class="time">Now</h3>
+    <img class="icon" src="${createIconUrl(iconNow)}"/>
+    <p class="hourly-temp">${formatTemperature(tempNow)}</p>
+</article>
+    `;
+
     for (let{temp,icon,dt_txt} of dataFor12Hours){
         innerHTMLString+=`
         <article>
-        <h3 class="time">${dt_txt.split(" ")[1]}</h3>
+        <h3 class="time">${timeFormatter.format(new Date(dt_txt))}</h3>
         <img class="icon" src="${createIconUrl(icon)}"/>
         <p class="hourly-temp">${formatTemperature(temp)}</p>
     </article>
@@ -56,6 +74,8 @@ const loadHourlyForecast=(hourlyForecast)=>{
     }
     hourlyContainer.innerHTML=innerHTMLString;
 }
+
+//merge min,max temp for a particular date
 const calculateDayWiseForecast=(hourlyForecast)=>{
     let dayWiseForecast=new Map();
     for(let forecast of hourlyForecast){
@@ -80,6 +100,7 @@ const calculateDayWiseForecast=(hourlyForecast)=>{
    return dayWiseForecast;
 }
 
+//put the data required in 5Day forecast
 const loadFiveDayForecast=(hourlyForecast)=>{
    const dayWiseForecast=calculateDayWiseForecast(hourlyForecast);
    const container=document.querySelector(".five-day-forecast-container");
@@ -109,14 +130,74 @@ const loadHumidity=({main:{humidity}})=>{
     container.querySelector(".humidity-value").textContent =`${humidity}%`;
 }
 
+const loadForecastUsingGeoLocation=()=>{
+    navigator.geolocation.getCurrentPosition(({coords})=>{
+        const {latitude:lat,longitude:lon}=coords;
+        selectedCity={lat,lon};
+        // console.log(selectedCity);
+        loadData();
+    },error=>console.log(error))
+}
+
+const loadData=async()=>{
+    const currentWeather= await getCurrentWeatherData(selectedCity);
+    // console.log(currentWeather);
+    loadCurrentForecast(currentWeather);
+    const hourlyForecast=await getHourForecast(currentWeather);
+    loadHourlyForecast(currentWeather,hourlyForecast);
+    loadFiveDayForecast(hourlyForecast);
+    loadFeelsLike(currentWeather);
+    loadHumidity(currentWeather);
+}
+
+
+function debounce(func){
+    let timer;
+    return (...args)=>{
+        clearTimeout(timer); //clear existing timeout
+        //create a new time till the user is timing
+        timer=setTimeout(()=>{
+            func.apply(this,args)
+        },500);
+    }
+}
+const onSearchChange=async(event)=>{
+      let {value}=event.target;
+      if(!value){
+        selectedCity=null;
+        selectedCityText="";
+      }
+      if(value && selectedCityText!==value){
+      let listOfCities= await getCitiesUsingGeolocation(value);
+      let options="";
+      for(let {lat,long,name,state,country}of listOfCities){
+        options+=`
+        <option data-city-details='${JSON.stringify({lat,long,name})}' value="${name},${state},${country}"></option>
+        `
+      }
+      document.querySelector("#cities").innerHTML=options;
+    }
+}
+
+const handleCitySelection=(event)=>{
+    selectedCityText=event.target.value;
+    let options=document.querySelectorAll("#cities>option");
+    if(options?.length){
+        let selectedOption=Array.from(options).find(opt=>opt.value===selectedCityText);
+        selectedCity=JSON.parse(selectedOption.getAttribute("data-city-details"));
+        loadData();
+    }
+}
+
+const debounceSearch=debounce((event)=>{onSearchChange(event)});
 
 //call fuction to get response from the api by async await
 document.addEventListener("DOMContentLoaded",async()=>{
-       const currentWeather= await getCurrentWeatherData();
-       loadCurrentForecast(currentWeather );
-       const hourlyForecast=await getHourForecast(currentWeather);
-       loadHourlyForecast(hourlyForecast);
-       loadFiveDayForecast(hourlyForecast);
-       loadFeelsLike(currentWeather);
-       loadHumidity(currentWeather);
+       loadForecastUsingGeoLocation();
+       const searchInput=document.querySelector("#search");
+       searchInput.addEventListener("input",debounceSearch);
+       searchInput.addEventListener("change",handleCitySelection);
+
  })
+
+ 
